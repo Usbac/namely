12604dc7d0cfebf7d6ac99ce264f29d644bce9c8
Usbac <38147742+Usbac@users.noreply.github.com>
2018-10-07 23:29:36 -0400

    Updated to Version 1.0
    Added a 'Delete Files' option.
    Added a Recursive option (for modifying files in the directory's subfolders).
    Code refactored.

--- Readme.md
+++ Readme.md
@@ -1,9 +1,9 @@
 # Namely
 <p align="center">
-<img src="http://usbac.com.ve/wp-content/uploads/2018/09/Namely-Poster.jpg" width="75%" height="75%"> 
+<img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/cdd8a170938599.5bb43a6702b70.png" width="75%" height="75%"> 
 </p>
 
-Massive file renamer utility made in Java using JavaFX
+Massive file renamer utility
 
 <a href="http://download1335.mediafire.com/q5a22yi7i53g/8dpfz3x0b4ea2nd/Namely.jar">Download Namely (jar)</a>
 
@@ -15,5 +15,6 @@ Massive file renamer utility made in Java using JavaFX
 * Multiple options for modifying your file's name (Invert the text, change order based in a separator, change the extension, switch between lowercases/uppercases and replace text).
 * Ability to filter which files will be modified, based in a Regular Expression.
 * Massive changes under the indicated directory or folder.
+* Option to modify files in the directory's subfolders.
 * Cross-platform with support for Windows, Mac OS and Linux.
 * And more...
---
+++ src/namely/ListController.java
@@ -0,0 +1,140 @@
+package namely;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import javafx.scene.control.DatePicker;
+
+public class ListController {
+    private final MainController main;
+    private final SimpleDateFormat date;
+    protected boolean showFile, isSpaceInChangeOrder, recursive;
+    protected int casesOptionSelected;
+    private DateFilter dateFilter;
+    private SizeFilter sizeFilter;
+    
+    private enum DateFilter {
+        OLDER,
+        NEWER
+    }
+    
+    protected enum SizeFilter {
+        SMALLER,
+        BIGGER
+    }
+    
+    
+    public ListController(MainController main) {
+        this.main = main;
+        date = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        recursive = true;
+    }
+    
+    
+    public void setDateFilter(String date) {
+        dateFilter = (date.equals("Older than"))? 
+            DateFilter.OLDER : DateFilter.NEWER;
+    }
+    
+    
+    public void setSizeFilter(String size) {
+        sizeFilter = (size.equals("Smaller than"))? 
+            SizeFilter.SMALLER : SizeFilter.BIGGER;
+    }
+    
+    
+    public void updateListView(boolean preview) {
+        if (main.directory != null) {
+            main.folderPath.setText(main.directory.getPath());
+        }
+        main.folder = new File(main.folderPath.getText());
+        if (main.folder.isDirectory()) {
+            main.table.getItems().clear();
+            ListFiles(main.folder.listFiles(), preview);
+        }
+    }
+    
+    
+    public void ListFiles(File[] listOfFiles, boolean preview) {
+        for (File file: listOfFiles) {
+            if (file.isFile()) {
+                showFile = true;
+                String name = preview? 
+                    getFilePreview(file, false).getName():file.getName();
+                SingleFile newfile = new SingleFile(name, 
+                                                    date.format(file.lastModified()), 
+                                                    FileFunctions.getSizeInKb(file)+" kB");
+                if (showFile)
+                    main.table.getItems().add(newfile);
+            } else if (recursive) {
+                ListFiles(file.listFiles(), preview);
+            }
+        }
+    }
+    
+    
+    public File getFilePreview(File aux, boolean deleteFile) {
+        String regex = main.regexInput.getText();
+        int selectedTab = main.tabPane.getSelectionModel().getSelectedIndex();
+        if ((!regex.isEmpty() && FileFunctions.getNameNoExtension(aux)!=null && FileFunctions.getNameNoExtension(aux).matches(regex)) || 
+             (regex.isEmpty() && FileFunctions.getNameNoExtension(aux)!=null) || selectedTab == 4)
+            switch (selectedTab) {
+                case 0: 
+                    return FileFunctions.inverse(aux);
+                case 1: 
+                    char charSeparator = (main.separator.getText().length() > 0)? 
+                            main.separator.getText().charAt(0): ' ';
+                    return FileFunctions.change_order(aux, charSeparator, isSpaceInChangeOrder);
+                case 2:
+                    return FileFunctions.cases(aux, casesOptionSelected);
+                case 3:
+                    return FileFunctions.replace(aux, main.renameOriginal.getText(), main.renameReplacement.getText());
+                case 4:
+                    showFile = deleteFile(aux, deleteFile);
+            }
+        return aux;
+    }
+    
+        
+    public boolean deleteFile(File file, boolean delete) {
+        boolean fileMatchesFields = true;
+        String regex = main.regexInput.getText();
+        String fileExtension = FileFunctions.getExtension(file.getName())
+                                            .substring(1);
+        float fileSize = Float.parseFloat(FileFunctions.getSizeInKb(file));
+        long fileModified = file.lastModified();
+        //If file doesn't matches the indicated extension 
+        if (!main.extensionField.getText().isEmpty() && !main.extensionField.getText().matches(fileExtension))
+            fileMatchesFields = false;
+        //If file doesn't matches Date filter (Older or Newer than the indicated date)
+        if (main.datePicker.getValue()!=null && !main.datePicker.getValue().toString().isEmpty()) {
+            if ((dateFilter == DateFilter.NEWER && fileModified < getDateInMilli(main.datePicker)) ||
+                (dateFilter == DateFilter.OLDER && fileModified > getDateInMilli(main.datePicker)))
+                fileMatchesFields = false;
+        }
+        //If file doesn't matches Size filter (Smaller or Bigger than the indicated size)
+        if (!main.sizeField.getText().isEmpty() && main.sizeField.getText().chars().allMatch(Character::isDigit)) {
+            float comparativeSize = Float.parseFloat(main.sizeField.getText());
+            if ((sizeFilter == SizeFilter.SMALLER && fileSize > comparativeSize) || 
+                (sizeFilter == SizeFilter.BIGGER && fileSize < comparativeSize))
+                fileMatchesFields = false;
+        }
+        //If file doesn't matches de Regex
+        if (!regex.isEmpty() && FileFunctions.getNameNoExtension(file)!=null && !FileFunctions.getNameNoExtension(file).matches(regex))
+            fileMatchesFields = false;
+        
+        if (delete && fileMatchesFields) 
+            file.delete();
+        return fileMatchesFields;
+    }
+    
+    
+    public long getDateInMilli(DatePicker d) {
+        return LocalDate.of(d.getValue().getYear(), d.getValue().getMonth(), d.getValue().getDayOfMonth())
+                        .atStartOfDay(ZoneOffset.UTC)
+                        .toInstant()
+                        .toEpochMilli();
+    }
+   
+}
